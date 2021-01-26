39b4dd72a3ec078a934a884fc2eed3c89cd5e03f
Anton Kahn <freelead@gmail.com>
2021-01-25 21:19:05 -0700

    Added slider for the works section

--- js/clients.js
+++ js/clients.js
@@ -1,6 +1,12 @@
 $(document).ready(function () {
+  $('.works-slider').slick({
+    dots: true,
+    dotsClass: 'slick-dots works__slider-dots',
+  })
+
   $('.clients__slider').slick({
     dots: true,
+    dotsClass: 'slick-dots clients__slider-dots',
     adaptiveHeight: true,
   })
 })
